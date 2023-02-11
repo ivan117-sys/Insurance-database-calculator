ea0a4682d9065404d35d87a7f95f8492e211a356
ivan117-sys <ivanmuskovic@gmail.com>
2023-02-11 13:29:46 +0100

    json

--- server.js
+++ server.js
@@ -26,12 +26,12 @@ app.listen(
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
 
-if (
-  process.env.NODE_ENV === 'production' ||
-  process.env.NODE_ENV === 'staging'
-) {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-  });
-}
+// if (
+//   process.env.NODE_ENV === 'production' ||
+//   process.env.NODE_ENV === 'staging'
+// ) {
+//   app.use(express.static('client/build'));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/client/build/index.html'));
+//   });
+// }
